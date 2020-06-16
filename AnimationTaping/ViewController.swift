8d7a9c5ea5190e262f06baf449e68ea5aae61192
Nina <ciz0@bk.ru>
2020-06-16 15:37:38 +0300

    Initial Commit

---
+++ AnimationTaping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationTaping
+//
+//  Created by Нина Ефимова on 16.06.2020.
+//  Copyright © 2020 Efim Nina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
